bb7217bd6e15f203d84006ce299fea995af76f29
Darryl L. Miles <darryl.miles@darrylmiles.org>
2023-03-07 19:54:29 +0000

    GradleVersionCompatibility 8.0.2 thru 5.6

--- src/integrationTest/kotlin/org/unbrokendome/gradle/plugins/xjc/GradleVersionCompatibilityIntegrationTest.kt
+++ src/integrationTest/kotlin/org/unbrokendome/gradle/plugins/xjc/GradleVersionCompatibilityIntegrationTest.kt
@@ -17,7 +17,7 @@ import org.unbrokendome.gradle.plugins.xjc.testutil.runGradle
 import java.io.File
 
 
-const val AllGradleVersions = "6.6.1, 6.5.1, 6.4.1, 6.3, 6.2.2, 6.1.1, 6.0.1, 5.6.4, 5.6"
+const val AllGradleVersions =  "8.0.2, 7.6.1, 7.5.1, 7.4.2, 7.3.3, 7.2, 7.1.1, 7.0.2, 6.9.4, 6.8.2, 6.7.1, 6.6.1, 6.5.1, 6.4.1, 6.3, 6.2.2, 6.1.1, 6.0.1, 5.6.4, 5.6"
 
 
 @GradleIntegrationTest
