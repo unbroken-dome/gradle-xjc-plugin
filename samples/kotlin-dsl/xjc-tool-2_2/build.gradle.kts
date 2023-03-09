5d0990e19cc6f89b04da0d494cabd13db2ecaf74
Darryl L. Miles <darryl.miles@darrylmiles.org>
2023-03-09 15:58:58 +0000

    Added: samples/xjc-tool-2_2 + IT

---
+++ samples/kotlin-dsl/xjc-tool-2_2/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    java
+    id("org.unbroken-dome.xjc") version "2.1.0-SNAPSHOT"
+}
+
+
+repositories {
+    mavenCentral()
+}
+
+
+xjc {
+    xjcVersion.set("2.2")
+}
+
+
+dependencies {
+    implementation("javax.xml.bind:jaxb-api:2.2.12")
+
+    xjcTool("javax.xml.bind:jaxb-api:2.2.12")
+
+    xjcTool("com.sun.xml.bind:jaxb-xjc:2.2.11")
+    xjcTool("com.sun.xml.bind:jaxb-impl:2.2.11")
+    xjcTool("com.sun.xml.bind:jaxb-core:2.2.11")
+}
