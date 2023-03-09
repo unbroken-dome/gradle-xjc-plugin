c000930d84c5862b4bb2d312e5be943f54f0d292
Darryl L. Miles <darryl.miles@darrylmiles.org>
2023-03-09 16:32:17 +0000

    Added: samples/xjc-version-default + IT

---
+++ samples/kotlin-dsl/xjc-version-default/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    java
+    id("org.unbroken-dome.xjc") version "2.1.0-SNAPSHOT"
+}
+
+
+repositories {
+    mavenCentral()
+}
+
+
+dependencies {
+    // 2.3 is the default xjcVersion
+    implementation("javax.xml.bind:jaxb-api:2.3.1")
+}
