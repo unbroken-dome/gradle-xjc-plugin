deb45d81922fdf212a25c6e216a181ca93022102
Darryl L. Miles <darryl.miles@darrylmiles.org>
2023-03-10 15:49:37 +0000

    Added samples/xjc-version-2_1-legacy + IT

---
+++ samples/kotlin-dsl/xjc-version-2_1-legacy/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    java
+    id("org.unbroken-dome.xjc") version "2.1.0-SNAPSHOT"
+}
+
+
+repositories {
+    mavenCentral()
+}
+
+
+xjc {
+    xjcVersion.set("2.1")
+}
+
+
+dependencies {
+    // This is still needed for Gradle to compile the generated Java and add transitively
+    implementation("javax.xml.bind:jaxb-api:2.1")
+}
